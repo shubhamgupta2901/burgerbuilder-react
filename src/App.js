4c106c4c97dc91324ba19fca5fff38288449771f
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-07-30 23:32:13 +0530

    Created Layout Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
-    <div >
-        
-    </div>
+    <Layout>
+      
+    </Layout>
   );
 }
 
