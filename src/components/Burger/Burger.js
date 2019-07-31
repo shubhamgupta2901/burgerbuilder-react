8c9b38939b9c0472d43b3e00888c7af72b9e5bc5
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-07-31 15:08:38 +0530

    Rendering Burger Ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,22 +4,37 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger =(props) => {
+    function renderIngredients(){
+        const burgerIngredients = [];
+        for(let[igName, igQuantity] of Object.entries(props.ingredients)){
+            for(let i = 0; i<igQuantity; i++){
+                burgerIngredients.push(<BurgerIngredient type={igName}/>);
+            }
+        }
+        return(
+            <React.Fragment>
+                {burgerIngredients}
+            </React.Fragment>
+            
+        );
+    }
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type={"bread-top"}/>
-            <BurgerIngredient type={"cheese"}/>
-            <BurgerIngredient type={"meat"}/>
-            <BurgerIngredient type={"bacon"}/>
-            <BurgerIngredient type={"salad"}/>
+            {renderIngredients()}
             <BurgerIngredient type={"bread-bottom"}/>
         </div>
     );
 }
 
 burger.propTypes ={
+    ingredients: PropTypes.object,
 }
 
 burger.defaultProps ={
+    ingredients:{
+        
+    }
 }
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import Burger from '../../components/Burger/Burger';
-class BurgerBuilder extends React.Component {
 
+class BurgerBuilder extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ingredients:{
+        salad:1,
+        bacon:1,
+        cheese:2,
+        meat:2  
+      }
+    }
+  }
   render(){
     return (
-        <React.Fragment>
-            <Burger/>
-            <div>Build Controls</div>
-        </React.Fragment>
+      <React.Fragment>
+        <Burger ingredients={this.state.ingredients}/>
+        <div>Build Controls</div>
+    </React.Fragment>
     );
   }
+}
+
+BurgerBuilder.propTypes ={
+}
+
+BurgerBuilder.defaultProps ={
 }
 
 export default BurgerBuilder;
