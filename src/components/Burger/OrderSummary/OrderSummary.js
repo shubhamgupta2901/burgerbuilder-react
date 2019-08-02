7ff88a88798d46c8a032ed9b1bc40f1866b583c2
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-02 11:49:16 +0530

    Converted OrderSummary to stateful component for lifecycle hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,11 +4,16 @@ import styles from './OrderSummary.module.css';
 import * as Utils from '../../../utils/Utils';
 import Button from '../../UI/Button/Button';
 
-const orderSummary =(props) => {
-    
-    const getIngredientSummary = () =>{
+class OrderSummary extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    getIngredientSummary = () =>{
         const ingredientSummary = [];
-        for(let [key, value] of Object.entries(props.ingredients)){
+        for(let [key, value] of Object.entries(this.props.ingredients)){
             if(value>0){
                 const summaryElement = <li key={Utils.generateUniqueId()}><span style={{textTransform: "capitalize"}}>{key}</span> : {value}</li>;
                 ingredientSummary.push(summaryElement);
@@ -16,34 +21,36 @@ const orderSummary =(props) => {
         }
         return ingredientSummary;
     }
-    
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>Delicious Burger with following contents:</p>
-            <ul>
-                {getIngredientSummary()}
-            </ul>
-            <p>Total price: ${props.totalPrice.toFixed(2)}.</p>
-            <p>Do you wish to continue?</p>
-            <Button onClick={props.onCancel} btnType={"Danger"}>Cancel</Button>
-            <Button onClick={props.onCancel} btnType={"Success"}>Continue</Button>
-        </React.Fragment>
-    );
+    render(){
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>Delicious Burger with following contents:</p>
+                <ul>
+                    {this.getIngredientSummary()}
+                </ul>
+                <p>Total price: ${this.props.totalPrice.toFixed(2)}.</p>
+                <p>Do you wish to continue?</p>
+                <Button onClick={this.props.onCancel} btnType={"Danger"}>Cancel</Button>
+                <Button onClick={this.props.onCancel} btnType={"Success"}>Continue</Button>
+            </React.Fragment>
+        );
+    }
 }
 
-orderSummary.propTypes ={
+OrderSummary.propTypes ={
     ingredients: PropTypes.object,
     totalPrice: PropTypes.number,
     onContinue:PropTypes.func,
     onCancel: PropTypes.func,
 }
 
-orderSummary.defaultProps ={
+OrderSummary.defaultProps ={
     ingredients: {},
     totalPrice: 0,
     onContinue: ()=>{},
     onCancel:()=>{},
 }
 
-export default orderSummary;
+export default OrderSummary;
+
