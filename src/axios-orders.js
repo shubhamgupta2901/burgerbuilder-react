5e0c439638992cab27f5de4ce19a44469b1280fe
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-06 12:29:18 +0530

    Added response and request logging interceptors to axios global and firebase instance

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -4,5 +4,24 @@ const instance = axios.create({
     baseURL: 'https://burger-builder-react-24a56.firebaseio.com/',
 });
 
+instance.interceptors.request.use(request =>{
+    console.log(request);
+    return request;
+},error=>{
+        console.log(error);
+        return Promise.reject(error);
+});
+
+
+instance.interceptors.response.use(response =>{
+    console.log(response);
+    return response;
+},error=>{
+        console.log(error);
+        return Promise.reject(error);
+});
+
+
+
 export default instance;
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request =>{
+    console.log(request);
+    return request;
+},error=>{
+        console.log(error);
+        return Promise.reject(error);
+});
+
+
+axios.interceptors.response.use(response =>{
+    console.log(response);
+    return response;
+},error=>{
+        console.log(error);
+        return Promise.reject(error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
