d4cff503d2c95754fe96e45e2f1091c86a6a9728
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-06 13:19:58 +0530

    Added Spinner UI Component

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Spinner.module.css';
+
+const spinner =(props) => (
+    <div className={styles.loader}>Loading...</div>
+);
+
+spinner.propTypes ={
+}
+
+spinner.defaultProps ={
+}
+
+export default spinner;
